37a1ba375d02ad870fb6277ef877b0cf6d8b4b84
Ayush Srivastava <ayushsrivastava9336@gmail.com>
2025-08-21 00:47:45 +0000

    make sthetoscope as it is

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -14,10 +14,9 @@ export function AppLogo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-        <path d="M4 14a2 2 0 1 0 4 0V6a6 6 0 1 0-8 0v4" />
-        <path d="M18 14a2 2 0 1 0 4 0V6a6 6 0 1 0-8 0v4" />
-        <path d="M6 4h12" />
-        <circle cx="12" cy="20" r="2" />
+      <path d="M12 2a5 5 0 0 0-5 5v1a4 4 0 0 0-4 4v2a4 4 0 0 0 4 4v1a5 5 0 0 0 5 5h0a5 5 0 0 0 5-5v-1a4 4 0 0 0 4-4v-2a4 4 0 0 0-4-4V7a5 5 0 0 0-5-5Z" />
+      <path d="M6 8.5V7a2 2 0 1 1 4 0v1.5" />
+      <path d="M14 8.5V7a2 2 0 1 1 4 0v1.5" />
     </svg>
   );
 }
